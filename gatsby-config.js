457c72700a6a8d2ab0976a438aea492d00e93fd7
ENSrUS <93621055+ENSrUS@users.noreply.github.com>
2021-12-01 18:28:22 -0500

    v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `clipinclout.ETH`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `clipinclout.eth`,
     author: `@clipinclout.eth`,
     siteUrl: `https://clipinclout.eth`,
   },
